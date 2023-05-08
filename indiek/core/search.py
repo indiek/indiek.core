a2f06ec67b1dad53bd14be5fe1c9addd341e7ab6
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2023-05-08 18:22:08 -0400

    filter_str unittest

--- indiek/core/search.py
+++ indiek/core/search.py
@@ -1,5 +1,6 @@
 """Search logic for the core IndieK API."""
-from typing import List, Union, Optional, Sequence, Dict
+import re
+from typing import List, Union, Sequence, Dict
 from indiek.core.items import Item, Definition, Theorem, Proof
 from indiek.mockdb.items import (Item as DBItem,
                                  Definition as DBDefinition,
@@ -8,13 +9,35 @@ from indiek.mockdb.items import (Item as DBItem,
 
 
 BackendItem = Union[DBItem, DBDefinition, DBTheorem, DBProof]
+ITEM_TYPES = Definition, Theorem, Proof
+
+
+def build_search_query(string: str) -> re.Pattern:
+    base_str = '('
+    base_str += '|'.join(string.split())
+    base_str += ')'
+    return re.compile(base_str, flags=re.IGNORECASE)
+
+
+def search_and_cast(query: re.Pattern, core_cls: Item) -> List[Item]:
+    """Trigger query on backend, fetch and cast to core objects.
+
+    Args:
+        query (re.Pattern): compiled regex object.
+        core_cls (Item): item class in core API to use for filtering.
+
+    Returns:
+        List[Item]: list of core items
+    """
+    db_cls = core_cls.BACKEND_CLS
+    return [core_cls.from_db(dbi) for dbi in db_cls.str_filter(query)]
 
 
 def fetch_and_cast(core_cls: Item) -> List[Item]:
     """Fetch all results from backend and cast to core objects.
 
     Args:
-        core_cls (Item): item class in core API
+        core_cls (Item): item class in core API to use for filtering.
 
     Returns:
         List[Item]: list of core items
@@ -23,22 +46,41 @@ def fetch_and_cast(core_cls: Item) -> List[Item]:
     return [core_cls.from_db(dbi) for dbi in db_cls.list_all()]
 
 
-
 def list_all_items(item_types: Sequence[Item] = (Definition, Theorem, Proof)) -> Dict[Item, List[Item]]:
     """Fetch all items with optional type filter.
 
     Args:
         item_types (Sequence[Item], optional): list-like of core item types to use for segmented
             search. For example, if item_types is [Proof, Theorem], then backend will be queried only for these
-            two types (more precisely for their corresponding types in backend typing). Defaults to None in which
-            case all types are queried. Note that the way to query all items without filtering on types is to set 
-            item_types to None (as opposed to setting it to [Item] which would throw a KeyError).
+            two types (more precisely for their corresponding types in backend typing). Defaults to (Definition,
+            Theorem, Proof) in which case all types are queried. Note that another way to query all items without filtering 
+            on types is to set item_types to an empty container.
 
     Returns:
-        Dict[Item, List[Item]]: segmented results. If item_types arg was None, then Item is the only key in the 
-            dict. Otherwise, returned dict has keys identical to item_types entries.
-
-    Raises:
-        KeyError: When item_types contains entries that are not keys of CORE_TO_DB_MAPPING.
+        Dict[Item, List[Item]]: segmented results. Returned dict has keys identical to item_types entries, with the
+            exception of the case where item_types was an empty container; for that latter case, returned dict has
+            the default keys (Definition, Theorem, Proof).
     """
+    if len(item_types) == 0:
+        item_types = ITEM_TYPES
     return {item_type: fetch_and_cast(item_type) for item_type in item_types}
+
+
+def filter_str(search_str: str, item_types: Sequence[Item] = (Definition, Theorem, Proof)) -> Dict[Item, List[Item]]:
+    """Search items from specified type which match search string.
+
+    Backend items of type compatible with item_types are searched, retrieved and cast to Core type.
+    The search_str is split on white spaces and regex are built and 'and-ed'. The resulting regex is
+    applied on item name and content.
+    
+    Args:
+        search_str (str): search string.
+        item_types (Sequence[Item]): list-like of core item types (similar to `list_all_items` arg of same name).
+    
+    Returns:
+        Dict[Item, List[Item]]: segmented results, only containing matching items.
+    """
+    if len(item_types) == 0:
+        item_types = ITEM_TYPES
+    query = build_search_query(search_str)
+    return {item_type: search_and_cast(query, item_type) for item_type in item_types}
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,7 @@
 import unittest
-from indiek.core.items import Item, Proof, Theorem, Definition
-from indiek.core.search import list_all_items
+import re
+from indiek.core.items import  Proof, Theorem, Definition
+from indiek.core.search import list_all_items, search_and_cast, build_search_query, filter_str
 
 
 CORE_ITEM_TYPES = [Proof, Theorem, Definition]
@@ -9,7 +10,25 @@ CORE_ITEM_TYPES = [Proof, Theorem, Definition]
 class TestSearch(unittest.TestCase):
     def setUp(self) -> None:
         """Make sure DB has at least 1 Item of each type."""
-        self.ids = {cls: cls().save() for cls in CORE_ITEM_TYPES}
+        self.ids = {cls: cls(name=str(cls)).save() for cls in CORE_ITEM_TYPES}
+
+    def test_list_all_items(self):
+        # all written items have 'class' in their name
+        class_in_name = filter_str("class")
+            
+        for written_cls in self.ids.keys():
+            loaded_item = written_cls.load(self.ids[written_cls])
+            self.assertIn(loaded_item, class_in_name[written_cls])
+        
+        # only Theorem and Proof items have 'theorem' or 'proof' word
+        thm_prf = filter_str('theorem proof')
+        
+        # check type-specific results contain expected ids from setUp
+        for core_type, core_items in thm_prf.items():
+            if core_type in {Theorem, Proof}:
+                self.assertTrue(core_items)
+            elif core_type == Definition:
+                self.assertFalse(core_items)
 
     def test_list_all_items(self):
         # at least written items from setUp should be present
@@ -36,6 +55,25 @@ class TestSearch(unittest.TestCase):
         for core_type, ikid in self.ids.items():
             self.assertIn(ikid, type_id_sets[core_type])
 
+    def test_search_and_cast(self):
+        thm_query = re.compile('Theorem')
+        thm_result = search_and_cast(thm_query, Theorem)
+
+        written_thm = Theorem.load(self.ids[Theorem])
+        self.assertIn(written_thm, thm_result)
+
+        written_proof = Proof.load(self.ids[Proof])
+        proof_result = search_and_cast(thm_query, Proof)
+        self.assertNotIn(written_proof, proof_result)
+
+    def test_build_search_query(self):
+        raw_str = r'user typed This'
+        query = build_search_query(raw_str)
+        base_str = '('
+        base_str += '|'.join(raw_str.split())
+        base_str += ')'
+        correct_query = re.compile(base_str, flags=re.IGNORECASE)
+        self.assertEqual(correct_query, query)
 
 if __name__ == '__main__':
     unittest.main()
